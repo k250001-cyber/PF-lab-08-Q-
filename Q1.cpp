b84c459b8b94938f298c7b9c6a8f0a99eda971da
Momin Faisal <k250001@nu.edu.pk>
2025-10-23 09:29:57 +0500

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	int i;
+printf("pyramid pattern\n");
+for( i=1;i<=5;i++){
+		printf("%d:",i);
+	for(int j=1; j<=10; j++){
+		printf("%3d",i*j);;
+	}	
+	printf("\n");
+	
+}	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
